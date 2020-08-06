d59d74fb6789f5e6af208b014dbc540f70adbd5b
Vinicius Pizetta de Souza <viniciuspzt@gmail.com>
2020-08-06 01:12:40 -0300

    create landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import Landing from './pages/Landing/index'
+
 import './assets/styles/global.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Landing/>
   );
 }
 
