801af06e8744543bc37ce26e5065307a5879d0fb
Vinicius Pizetta de Souza <viniciuspzt@gmail.com>
2020-08-27 00:55:18 -0300

    config db til first migration

---
+++ server/README.md
@@ -0,0 +1,11 @@
+#Funcionalidades
+
+##Conexões
+
+-Rota para listar total de conexões realizadas
+-Rota para criar nova conexão
+
+##Aulas
+-Rota para criar aulas
+-Rota para listar aulas
+-Filtrar por matéria dia da semana e horário
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,10 @@ import express from 'express'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-  return response.send('hello')
-})
+app.use(express.json())
 
+app.get('/', (request, response) => {
+  return response.json({message: 'hello'})
+})
 
 app.listen(3333)
