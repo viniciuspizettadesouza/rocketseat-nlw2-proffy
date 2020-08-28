92bc7c784aa0db3aa74875ab1f2cb2d5e6fa108e
Vinicius Pizetta de Souza <viniciuspzt@gmail.com>
2020-08-28 01:02:45 -0300

    list classes route

--- server/src/router.ts
+++ server/src/router.ts
@@ -5,6 +5,7 @@ const router = express.Router()
 
 const classesController = new ClassesController()
 
+router.get('/classes', classesController.index)
 router.post('/classes', classesController.create)
 
 export default router
