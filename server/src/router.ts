ba261a6de45de1eddb6141946dee0024d731df86
Vinicius Pizetta de Souza <viniciuspzt@gmail.com>
2020-08-28 01:16:07 -0300

    create and list connections

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import ClassesController from "./controllers/ClassesController";
+import ConnectionsController from "./controllers/ConnectionsController";
 
 const router = express.Router()
 
 const classesController = new ClassesController()
+const connectionsController = new ConnectionsController()
 
 router.get('/classes', classesController.index)
 router.post('/classes', classesController.create)
+
+router.get('/connections', connectionsController.index)
+router.post('/connections', connectionsController.create)
 
 export default router
