cc17b85facd954960cb5562583a843df408f44a6
Vinicius Pizetta de Souza <viniciuspzt@gmail.com>
2021-01-07 15:05:59 -0300

    create input component, pass description prop to page header, styles

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,26 +4,18 @@ import PageHeader from "../../assets/components/PageHeader";
 
 import './styles.css'
 import TeacherItem from "../../assets/components/TeacherItem";
+import Input from "../../assets/components/input";
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
+          <Input name="subject" label="Matéria"/>
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"/>
-          </div>
+          <Input name="week_day" label="Dia da semana"/>
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+          <Input type="time" name="time" label="Hora"/>
         </form>
       </PageHeader>
 
